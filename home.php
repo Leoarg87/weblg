8cfa1fe3f4b4d954283e1fff40e09c7f0f21d41c
Leoarg87 <leoarielgarcia87@gmail.com>
2022-05-06 14:52:39 +0200

    agregamos home.php

---
+++ home.php
@@ -0,0 +1,93 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<body>
+<section>
+    <div>
+        <div class=" d-flex h-75">
+            <div class="col-6 d-flex justify-content-center pt-5" id="presentacion" >
+                <p>
+                Con su permiso me presento, soy <strong>Leonardo Ariel Garcia</strong>, programador web y programador de
+                aplicaciones para moviles. <br><br> Resido en <strong>M&aacute;laga</strong>, España. Trabajo como
+                desarrollador freelance online para una multitud de personas y empresas repartidas por todo el mundo,
+                desde
+                Espa&ntilde;a, hasta empresas en Latino am&eacute;rica. Clientes con los que trabajo de forma remota.
+                <br><br>
+                Me considero una persona entusiasta, proactivo y perseverante, con muchas ganas de aprender. Me dedico a
+                la
+                programaci&oacute;n debido a que es una profesi&oacute;n con mucha salida laboral la cual te exige estar
+                siempre en continuo aprendizaje. Amante de la informatica y de las nuevas tecnologias decidi apostar por
+                el
+                mundo tecnologico ya que tiene muchisimas salidas laborales. <br><br>
+                Hablo Español de forma nativa y tambien Ingles nivel avanzado por si cualquier cliente lo requiere estoy
+                dispuesto a realizar trabajos en ingles. <br><br>
+                Me gusta trabajar como programador dado que te da mucha libertad tanto horaria como geografica por lo
+                cual
+                puedo realizar otras actividades y/o hobbies. <br><br>
+                Actualmente he creado varias paginas web de muchas empresas y locales comerciales como <a
+                    href="https://www.joyeriaaranda.com/"> Joyeria Aranda</a> , <a
+                    href="https://www.heladoslosvalencianoscoin.com/">Heladeria Los Valencianos</a> y muchas otras mas que
+                os
+                las enseñare encantado en mi apartado de Portafolio.</a> <br><br>
+                He estudiado un master de programaci&oacute;n de paginas web y aplicaciones moviles en la escuela de <a
+                    href="https://www.masterd.es/">Master D</a> que son pioneros en el sistema de enseñanzas tanto de
+                programadores como de tecnicos informaticos y con una amplia varierdad de masters y cursos.
+                </p>
+            </div>
+            <h3 class="notice">Noticias</h3>
+            <div id="noticias"class="col-5 pt-5 w-25 mw-100 h-200px mh-100 mt-25 overflow-auto">
+            
+                <?php include("noticias.php")?>
+            </div>
+        </div>
+        
+    </div>
+
+    <hr >
+    <div id="servicios">
+        <header>
+            <h2>Servicios:</h2><br>
+            <h3>Puedo ayudarte con las siguientes materias</h3><br>
+        </header>
+        <p>Como podr&aacute; ver puedo satisfacer gran cantidad de las necesidades de la empresa actual en lo que a
+            nuevas tecnolog&iacute;as se refiere, y siempre a un precio razonable. Creo firmemente que hay que adaptarse
+            a los nuevos tiempos tanto en tecnolog&iacute;a como en competitividad. Por eso ver&aacute; que mis
+            servicios y precios se ajustan a la nueva empresa, al emprendedor a los que saben lo que quieren y como lo
+            quieren.</p>
+    </div>
+    <hr>
+    <div id="cajas">
+        <div class="caja">
+            <img class="icono" src="img/ordenador.jpeg" alt="desarrollo web" width="40%">
+            <h4 class="head">
+                Desarrollo Web
+            </h4>
+            <p>Desarrollo web a medida</p>
+            <p>Programaci&oacute;n desde cero</p>
+            <p>Gestores de contenidos - CMS</p>
+            <p>Wordpress</p>
+            <p>Tiendas online</p>
+            <p>Dise&ntilde;o a medida, maquetaci&oacute;n</p>
+            <p>Blogs</p>
+        </div>
+        <div class="caja">
+            <img class="icono" src="img/movil.jpeg" alt="desarrollo aplicaciones moviles" width="40%">
+            <h4 class="head">
+                Aplicaciones moviles
+            </h4>
+            <p>Creaci&oacute;n, desarrollo y mantenimiento de aplicaci&oacute;nes m&oacute;viles multiplataforma.</p>
+            <p>Android, IPhone, Firefox OS y Windows Mobile.</p>
+            <p>PhoneGap, APPcelerator, Desarrollo nativo.</p>
+        </div>
+        <div class="caja1">
+            <img class="icono1" src="img/redes.png" alt="Community Manager" width="70%">
+            <h4 class="head1">
+                Community Manager
+            </h4>
+            <p>Construir, gestionar y administrar la comunidad online .</p>
+            <p>Gestiono todo tipo de redes sociales (facebook, instagram, google, twitter).</p>
+            <p>Responder a la comunidad online.</p>
+            <p>Diseñar nuevos contenidos para tus redes sociales.</p>
+        </div>
+    </div>
+    </section>
+    
+</body>
